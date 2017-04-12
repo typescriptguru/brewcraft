2ca1dd772a76c657ffef773314d6577a8efc84b3
WBIT SALE <WBIT SALE>
2017-04-12 17:53:52 +0800

    test

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/brewcraft/client
-sudo npm install
+# cd /home/ec2-user/brewcraft/client
+# sudo npm install
 # sudo npm install node-sass
 # sudo npm run build
 # cd /home/ec2-user/brewcraft/server

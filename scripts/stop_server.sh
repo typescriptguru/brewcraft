d8c4b5abc06d546420fbe1a874b7c248d49d7c9f
WBIT SALE <WBIT SALE>
2017-04-13 16:20:11 +0800

    test

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo pm2 delete all
-cd /home/ec2-user/brewcraft/client
-sudo rm -r node_modules dist
-cd /home/ec2-user/brewcraft/server
-sudo rm -r node_modules
+# sudo pm2 delete all
+# cd /home/ec2-user/brewcraft/client
+# sudo rm -r node_modules dist
+# cd /home/ec2-user/brewcraft/server
+# sudo rm -r node_modules

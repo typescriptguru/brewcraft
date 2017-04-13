442c6863f0b20ab362ad1735af80f2e0945f8940
WBIT SALE <WBIT SALE>
2017-04-13 20:51:48 +0800

    update appspec.yml

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+sudo npm install -g @angular/cli
+sudo npm install -g pm2
 cd /home/ec2-user/brewcraft/client
 sudo npm install
 sudo npm install node-sass
 sudo ng build
 cd /home/ec2-user/brewcraft/server
 sudo npm install
+sudo pm2 delete all
+sudo pm2 start server.js

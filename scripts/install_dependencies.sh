62b189656c71d9ab3beb9ddbf0676eafbfbdf47a
WBIT SALE <WBIT SALE>
2017-04-13 15:37:09 +0800

    fix ui of auth pages

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+cd /home/ec2-user/brewcraft/client
+sudo npm install
+sudo npm install node-sass
+sudo ng build
+cd /home/ec2-user/brewcraft/server
+sudo npm install
+sudo pm2 start server.js
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo pm2 delete all
+cd /home/ec2-user/brewcraft/client
+sudo rm -r node_modules dist
+cd /home/ec2-user/brewcraft/server
+sudo rm -r node_modules

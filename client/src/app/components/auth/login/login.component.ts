0b8638fc12eb390023e667a7e143979ee22d5e34
WBIT SALE <WBIT SALE>
2017-04-13 15:19:44 +0800

    Fix UI of Auth pages

--- client/src/app/components/auth/login/login.component.ts
+++ client/src/app/components/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService, User} from '../../../services/auth.service';
+import { AuthService, User } from '../../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
@@ -12,22 +12,35 @@ export class LoginComponent implements OnInit {
   user: User = new User();
   isSignup: Boolean = false;
 
+  onPassword: Boolean = true;
+  onEmail: Boolean = true;
+
   constructor(private authService: AuthService,
-    public flashMessage: FlashMessagesService,) { }
+    public flashMessage: FlashMessagesService, ) { }
 
   ngOnInit() {
   }
 
   login() {
+    console.log(this.user);
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.onPassword = true;
+    if (this.user.password == "" || this.user.password == null) {
+      this.onPassword = false;
+    }
+    this.onEmail = re.test(this.user.email);
+    if (this.onPassword === true && this.onEmail === true) {
       var self = this;
       this.authService.loginWithEmailAndPassword(this.user)
-      .then((res) => {
-        self.flashMessage.show('You have been successfully logged in ',
-          { cssClass: 'alert-success', timeout: 5000 });
-      })
-      .catch(function(error) {
-        self.flashMessage.show(error.message,
-          { cssClass: 'alert-danger', timeout: 5000 });
-      });
+        .then((res) => {
+          self.flashMessage.show('You have been successfully logged in ',
+            { cssClass: 'alert-success', timeout: 5000 });
+        })
+        .catch(function (error) {
+          self.flashMessage.show(error.message,
+            { cssClass: 'alert-danger', timeout: 5000 });
+          self.user.password = "";
+        });
+    }
   }
 }
--- client/src/app/components/auth/signup/signup.component.ts
+++ client/src/app/components/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService, User} from '../../../services/auth.service';
+import { AuthService, User } from '../../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
@@ -12,20 +12,49 @@ export class SignupComponent implements OnInit {
   user: User = new User();
   isSignup: Boolean = false;
 
+  emptyPassword: Boolean = false;
+  matchPassword: Boolean = true;
+  validPassword: Boolean = true;
+  emptyUsername: Boolean = false;
+  onEmail: Boolean = true;
+
   constructor(private authService: AuthService,
-    public flashMessage: FlashMessagesService,) { }
+    public flashMessage: FlashMessagesService, ) { }
 
   ngOnInit() {
   }
 
-  signup() {
+  signup(username, email, password, confirmpassword) {
+
+    console.log(password, confirmpassword)
+    this.matchPassword = true;
+    this.validPassword = true;
+    this.emptyUsername = false;
+    this.onEmail = true;
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.onEmail = re.test(this.user.email);
+
+    if(username == "" || username == undefined) 
+      this.emptyUsername = true;
+
+
+    re = /^(?=.*[A-Z])(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,}$/;
+    this.validPassword = re.test(password);
+
+    if(password != confirmpassword && this.validPassword)
+      this.matchPassword = false;
+
+    if (this.onEmail && this.matchPassword && this.validPassword && !this.emptyPassword) {
+      var self = this;
       this.authService.signupWithEmailAndPassword(this.user)
-      .then((res) => {
-        this.flashMessage.show('You have been successfully registered ',
-          { cssClass: 'alert-success', timeout: 3000 });
-      })
-      .catch(function(error) {
-        console.log(error)
-      });
+        .then((res) => {
+          this.flashMessage.show('You have been successfully registered ',
+            { cssClass: 'alert-success', timeout: 3000 });
+        })
+        .catch(function (error) {
+          self.flashMessage.show(error.message,
+            { cssClass: 'alert-danger', timeout: 5000 });
+        });    }
+
   }
 }
